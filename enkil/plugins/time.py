8ae8e8690d4cdf811a139bcf473c4acd657cec83
AndrewD <andrew_j_dunham@hotmail.com>
2012-05-23 00:04:01 +0200

    Updated time plugin

--- enkil/plugins/time.py
+++ enkil/plugins/time.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import time
+from datetime import datetime
 
 from enkil.log import getLogger
 from enkil.main import InfoClass
@@ -12,15 +13,32 @@ class CurrTime(InfoClass):
     DEPENDENCIES = None
 
     def get(self, info):
+        now = datetime.now()
+        utcnow = datetime.utcnow()
         return {
-            "localtime": time.time(),
+            "localtime": now.ctime(),
+            "gmtime": utcnow.ctime(),
+            "localisotime": now.isoformat(),
+            "gmisotime": utcnow.isoformat(),
+
+            "time": now.strftime("%H%M%S"),
+            "date": now.strftime("%Y%m%d"),
+        }
+
+
+class TimeZone(InfoClass):
+    DEPENDENCIES = None
+
+    def get(self, info):
+        return {
             "dst": True if time.daylight == 1 else False,
-            "gmtime": time.mktime(time.gmtime()),
-            "ctime": time.ctime(),
+            "tzname": time.tzname[1] if time.daylight != 0 else time.tzname[0],
+            "tzoffset": time.altzone if time.daylight != 0 else 0,
         }
 
 
 def getHandlers(info):
     return [
-        CurrTime
+        CurrTime,
+        TimeZone
     ]
